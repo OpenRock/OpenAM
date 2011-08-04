4d75a7c5e0bef4adbe5a64ed4f6304b9a0baeadb
steve <steve@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2011-08-04 09:19:10 +0000

    OPENAM-627
    git-svn-id: https://svn.forgerock.org/openam/trunk@978 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- opensso/products/amserver/source/org/forgerock/openam/setup/OpenAMUpgrade.java
+++ opensso/products/amserver/source/org/forgerock/openam/setup/OpenAMUpgrade.java
@@ -1,7 +1,7 @@
 /**
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright (c) 2011 ForgeRock AS. All Rights Reserved
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -9,33 +9,27 @@
  * compliance with the License.
  *
  * You can obtain a copy of the License at
- * https://opensso.dev.java.net/public/CDDLv1.0.html or
- * opensso/legal/CDDLv1.0.txt
+ * http://forgerock.org/license/CDDLv1.0.html
  * See the License for the specific language governing
  * permission and limitations under the License.
  *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
- * at opensso/legal/CDDLv1.0.txt.
+ * at http://forgerock.org/license/CDDLv1.0.html
  * If applicable, add the following below the CDDL Header,
  * with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: OpenSSOConfigurator.java,v 1.4 2009/08/11 23:50:42 goodearth Exp $
  */
 
-/*
- * Portions Copyrighted 2011 ForgeRock AS
- */
-
-package com.sun.identity.setup;
+package org.forgerock.openam.setup;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
@@ -45,23 +39,19 @@ import java.util.Enumeration;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
-public class OpenSSOConfigurator {
-
-    private static final String OPENSSO_CONFIGURATOR_PROPERTIES =
-        "OpenSSOConfigurator";
+/**
+ *
+ * @author steve
+ */
+public class OpenAMUpgrade {
+    private static final String OPENAM_UPGRADE_PROPERTIES =
+        "OpenAMUpgrade";
     private static final String SERVER_URL = "SERVER_URL";
-    static final String DEPLOYMENT_URI = "DEPLOYMENT_URI";
-    private static final String ADMIN_PWD = "ADMIN_PWD";
-    private static final String ADMIN_CONFIRM_PWD = "ADMIN_CONFIRM_PWD";
-    private static final String AMLDAPUSERPASSWD = "AMLDAPUSERPASSWD";
-    private static final String AMLDAPUSERPASSWD_CONFIRM =
-        "AMLDAPUSERPASSWD_CONFIRM";
-    private static final String USERSTORE_TYPE = "USERSTORE_TYPE";
+    private static final String DEPLOYMENT_URI = "DEPLOYMENT_URI";
 
     public static void main(String[] args) {
-
         ResourceBundle rb = ResourceBundle.getBundle(
-            OPENSSO_CONFIGURATOR_PROPERTIES);
+            OPENAM_UPGRADE_PROPERTIES);
 
         if ((args.length != 2) ||
             (!(args[0].equals("--file") || args[0].equals("-f")))) {
@@ -71,9 +61,7 @@ public class OpenSSOConfigurator {
         }
 
         String configFile = args[1];
-
         Properties config = new Properties();
-
         FileInputStream fis = null;
         
         try {
@@ -91,49 +79,24 @@ public class OpenSSOConfigurator {
             }
         }
 
-
         String serverURL = null;
         String deploymentURI = null;
-        String userStoreType = null;
 
-        StringBuilder postBodySB = new StringBuilder();
         for(Enumeration e = config.keys(); e.hasMoreElements() ;) {
-            String key = (String)e.nextElement();
-            String val = (String)config.get(key);
+            String key = (String) e.nextElement();
+            String val = (String) config.get(key);
 
             if (val != null) {
                 val = val.trim();
+                
                 if (val.length() > 0) {
                     if (key.equals(DEPLOYMENT_URI)) {
                         deploymentURI = val;
-                    } else {
-                        if (key.equals(SERVER_URL)) {
-                            serverURL = val;
-                        }
-                        if (postBodySB.length() > 0) {
-                            postBodySB.append("&");
-                        }
-                        String encodedVal = null;
-                        try {
-                            encodedVal = URLEncoder.encode(val, "UTF-8");
-                        } catch (UnsupportedEncodingException ueex) {
-                            encodedVal = val;
-                        }
-                        postBodySB.append(key).append("=").append(encodedVal);
-                        if (key.equals(ADMIN_PWD)) {
-                            postBodySB.append("&").append(ADMIN_CONFIRM_PWD)
-                                .append("=").append(encodedVal);
-                        } else if (key.equals(AMLDAPUSERPASSWD)) {
-                            postBodySB.append("&")
-                                .append(AMLDAPUSERPASSWD_CONFIRM)
-                                .append("=").append(encodedVal);
-                        }
+                    } else if (key.equals(SERVER_URL)) {
+                        serverURL = val; 
                     }
-                    if (key.equals(USERSTORE_TYPE)) {
-                       userStoreType = val;
-                    } 
                 }
-            }    
+            }
         }
 
         if (serverURL == null) {
@@ -154,45 +117,37 @@ public class OpenSSOConfigurator {
             serverURL = serverURL.substring(0, serverURL.length() - 1);
         }
 
-        String openssoURL = serverURL + deploymentURI;
+        String openamURL = serverURL + deploymentURI;
 
-        ReadProgress rp = new ReadProgress(openssoURL);
+        ReadProgress rp = new ReadProgress(openamURL);
         Thread t = new Thread(rp);
         t.start();
 
         DataOutputStream os = null;
         BufferedReader br = null;
+        
         try {
-            URL url = new URL(openssoURL + "/config/configurator");
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
+            URL url = new URL(openamURL + "/config/upgrade/upgrade.htm?actionLink=doUpgrade");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
             conn.setUseCaches(false);
-            conn.setRequestProperty("Content-Length", Integer.toString(
-                postBodySB.length()));
-            conn.setRequestProperty("Content-Type",
-                "application/x-www-form-urlencoded");
             conn.connect();
 
-            os = new DataOutputStream(conn.getOutputStream());
-            os.writeBytes(postBodySB.toString());
-            os.flush();
-
-
             int responseCode = conn.getResponseCode();
+            
             if (responseCode == 200) {
                 br = new BufferedReader(new InputStreamReader(
                     conn.getInputStream()));
                 String str = null;
                 while ((str = br.readLine()) != null) {
-                    System.out.println(str);
+                    if (str.equals("true")) {
+                        System.out.println("\nUpgrade Complete.");
+                    } else {
+                        System.out.println("\nUpgrade Complete. Please check the amUpgrade debug file for errors");
+                    }
                 }
             } else {
-                System.out.println(rb.getString("configFailed"));
-                if ((userStoreType != null) && 
-                    (userStoreType.equals("LDAPv3ForADDC"))) {
-                    System.out.println(rb.getString("cannot.connect.to.UM.datastore"));
-                }
+                System.out.println(rb.getString("upgradeFailed"));
             }
             conn.disconnect();
         } catch (ProtocolException ex) {
@@ -215,14 +170,15 @@ public class OpenSSOConfigurator {
                 }
             }
         }
+        
         System.exit(0);
     }
 
     static class ReadProgress implements Runnable {
-        String openssoURL = null;
+        String openamURL = null;
 
-        public ReadProgress(String openssoURL) {
-            this.openssoURL = openssoURL;
+        public ReadProgress(String openamURL) {
+            this.openamURL = openamURL;
         }
 
         public void run() {
@@ -231,9 +187,10 @@ public class OpenSSOConfigurator {
             while (retry <= 1) {
                 retry++;
                 BufferedReader br = null;
+                
                 try {
-                    URL url = new URL(openssoURL +
-                        "/setup/setSetupProgress?mode=text");
+                    URL url = new URL(openamURL +
+                        "/upgrade/setUpgradeProgress?mode=text");
                     HttpURLConnection conn =
                         (HttpURLConnection)url.openConnection();
                     conn.setInstanceFollowRedirects(false);
@@ -273,5 +230,5 @@ public class OpenSSOConfigurator {
                 break;
             }
         }
-    }
+    }    
 }
