b7ff88a2f883e2a1ad0fcb5bcbb2d88aa3634a9b
peter.major <peter.major@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2011-10-07 17:51:46 +0000

    Fix for OPENAM-792
    git-svn-id: https://svn.forgerock.org/openam/trunk@1255 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- opensso/products/federation/openfm/source/com/sun/identity/workflow/ImportSAML2MetaData.java
+++ opensso/products/federation/openfm/source/com/sun/identity/workflow/ImportSAML2MetaData.java
@@ -138,6 +138,7 @@ public class ImportSAML2MetaData {
         SAML2MetaSecurityUtils.verifySignature(doc);
         workaroundAbstractRoleDescriptor(doc);
         Object obj = SAML2MetaUtils.convertNodeToJAXB(doc);
+        obj = workaroundJAXBBug(obj);
 
         return (obj instanceof EntityDescriptorElement) ?
             (EntityDescriptorElement)obj : null;
@@ -182,5 +183,16 @@ public class ImportSAML2MetaData {
             }
         }
     }
+
+    private static Object workaroundJAXBBug(Object obj) throws JAXBException {
+        String metadata = SAML2MetaUtils.convertJAXBToString(obj);
+        String replaced = metadata.replaceAll("<(.*:)?Extensions/>", "");
+        if (metadata.equalsIgnoreCase(replaced)) {
+            return obj;
+        } else {
+            return SAML2MetaUtils.convertStringToJAXB(replaced);
+        }
+    }
+
 }
 
