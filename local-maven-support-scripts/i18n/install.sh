7e424b9ec789a39bad6907b0bfa2db0890615329
jeff.schenk <jeff.schenk@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2012-06-08 18:32:56 +0000

    additional cleanup in products.properties
    git-svn-id: https://svn.forgerock.org/openam/branches/maven_branch_01@2117 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- local-maven-support-scripts/i18n/install.sh
+++ local-maven-support-scripts/i18n/install.sh
@@ -3,7 +3,7 @@ mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
                        -Dfile=i18n-core-1.4.0.jar \
                        -DgroupId=external \
-                       -DartifactId=i18-core \
+                       -DartifactId=i18n-core \
                        -Dversion=1.4.0 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
