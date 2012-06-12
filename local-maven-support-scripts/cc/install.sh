471ea58631bd11eb1985cb206ce7abe731e06e54
jeff.schenk <jeff.schenk@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2012-06-12 23:01:23 +0000

    Move over lockhart com.sun.web.ui artifacts, add to local Maven repo. Eventually will be pushed into a ForgeRock Common Repository.
    git-svn-id: https://svn.forgerock.org/openam/branches/maven_branch_01@2193 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- local-maven-support-scripts/cc/install.sh
+++ local-maven-support-scripts/cc/install.sh
@@ -4,7 +4,7 @@ mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -Dfile=cc.jar \
                        -DgroupId=com.sun.web.ui \
                        -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
@@ -14,7 +14,7 @@ mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -Dfile=cc_de.jar \
                        -DgroupId=com.sun.web.ui \
                        -DartifactId=cc_de \
-                       -Dversion=2005-08-08 \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
@@ -23,98 +23,98 @@ mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
                        -Dfile=cc_es.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_es \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_fr.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_fr \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_it.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_it \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_ja.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_ja \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_ko.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_ko \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_sv.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_sv \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_zh.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_zh \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_zh_CN.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_zh_CC \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_zh_HK.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_zh_HK \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
 
 mvn -e -X org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                        -DlocalRepositoryPath=../../local-maven-support-repository \
-                       -Dfile=cc.jar \
+                       -Dfile=cc_zh_TW.jar \
                        -DgroupId=com.sun.web.ui \
-                       -DartifactId=cc \
-                       -Dversion=2005-08-08 \
+                       -DartifactId=cc_zh_TW \
+                       -Dversion=2008-08-08 \
                        -Dpackaging=jar \
                        -DgeneratePom=true \
                        -DuniqueVersion=true
