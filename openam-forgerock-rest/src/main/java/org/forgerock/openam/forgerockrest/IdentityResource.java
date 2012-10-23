738ac974edd4b173d3a180c63dbbc8da73ab665b
alin <alin@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2012-10-23 23:07:10 +0000

    Changing pom file parent naming locally.
    git-svn-id: https://svn.forgerock.org/openam/branches/openam_10.1.0_alin@3440 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam-forgerock-rest/src/main/java/org/forgerock/openam/forgerockrest/IdentityResource.java
+++ openam-forgerock-rest/src/main/java/org/forgerock/openam/forgerockrest/IdentityResource.java
@@ -307,11 +307,6 @@ public final class IdentityResource implements CollectionResourceProvider {
         Token admin = new Token();
         admin.setId(adminToken.getTokenID().toString());
 
-        SSOToken adminToken = (SSOToken) AccessController.doPrivileged(
-                AdminTokenAction. getInstance());
-        Token admin = new Token();
-        admin.setId(adminToken.getTokenID().toString());
-
         try {
             IdentityServicesImpl idsvc = new IdentityServicesImpl();
             IdentityDetails dtls = idsvc.read(resourceId, iDSvcsAttrList, admin);
