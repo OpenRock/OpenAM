975ee87c11b4f921ff7fa3bcf8bd842cbe4a3283
jason <jason@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2012-10-23 16:28:26 +0000

    fixed unused import and mvn build error
    git-svn-id: https://svn.forgerock.org/openam/branches/OAuth2_Maven@3433 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam/openam-oauth2/openam-oauth2-restlet-extension/src/main/java/org/forgerock/restlet/ext/oauth2/provider/RegisterClient.java
+++ openam/openam-oauth2/openam-oauth2-restlet-extension/src/main/java/org/forgerock/restlet/ext/oauth2/provider/RegisterClient.java
@@ -37,14 +37,11 @@ import org.restlet.Context;
 import org.restlet.data.Form;
 import org.restlet.data.Reference;
 import org.restlet.data.Status;
-import org.restlet.ext.servlet.internal.ServletCall;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 import org.restlet.Request;
-
-import javax.servlet.http.HttpServletRequest;
 import java.security.AccessController;
 import java.util.*;
 
