b82ab257c685d8598ae5335cf38af5b0b206ff13
jason <jason@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2013-04-29 19:49:32 +0000

    AME-1706 CR-1621
    git-svn-id: https://svn.forgerock.org/openam/branches/openid_connect_implementation@5151 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam-oauth2/openam-oauth2-rest/src/main/java/org/forgerock/openam/oauth2/rest/TokenResource.java
+++ openam-oauth2/openam-oauth2-rest/src/main/java/org/forgerock/openam/oauth2/rest/TokenResource.java
@@ -1,18 +1,25 @@
 /*
- * Copyright (c) 2012 ForgeRock AS. All rights reserved.
+ * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
+ * Copyright (c) 2012-2013 ForgeRock Inc. All rights reserved.
  *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
  *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information: "Portions Copyrighted [2012] [ForgeRock Inc]".
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
  *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions copyright [year] [name of copyright owner]"
  */
 package org.forgerock.openam.oauth2.rest;
 
@@ -105,7 +112,11 @@ public class TokenResource implements CollectionResourceProvider {
                     new JsonResourceAccessor(repository, JsonResourceContext.newRootContext());
             try {
                 response = accessor.read(resourceId);
-                String username = response.get(OAuth2Constants.CoreTokenParams.USERNAME).asString();
+                Set<String> usernameSet = (Set<String>)response.get(OAuth2Constants.CoreTokenParams.USERNAME).getObject();
+                String username= null;
+                if (usernameSet != null && !usernameSet.isEmpty()){
+                    username = usernameSet.iterator().next();
+                }
                 if(username == null || username.isEmpty()){
                     PermanentException ex = new PermanentException(404, "Not Found", null);
                     handler.handleError(ex);
@@ -218,7 +229,10 @@ public class TokenResource implements CollectionResourceProvider {
                     new JsonResourceAccessor(repository, JsonResourceContext.newRootContext());
             try {
                 response = accessor.read(resourceId);
-                username = response.get(OAuth2Constants.CoreTokenParams.USERNAME).asString();
+                Set<String> usernameSet = (Set<String>)response.get(OAuth2Constants.CoreTokenParams.USERNAME).getObject();
+                if (usernameSet != null && !usernameSet.isEmpty()){
+                    username = usernameSet.iterator().next();
+                }
                 if(username == null || username.isEmpty()){
                     PermanentException ex = new PermanentException(404, "Not Found", null);
                     handler.handleError(ex);
